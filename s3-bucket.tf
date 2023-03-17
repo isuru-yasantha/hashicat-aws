015025752f5ddbb89b1aca8d0b33e60b043b3ded
Isuru Wickramarathne <isuru.yw@gmail.com>
2023-03-17 12:10:06 +1100

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,5 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
+module "s3-bucket" {
+  source  = "app.terraform.io/testorgisuru/s3-bucket/aws"
+  version = "2.8.0"
   bucket_prefix = var.prefix
-
-  versioning = {
-    enabled = true
-  }
-
 }
